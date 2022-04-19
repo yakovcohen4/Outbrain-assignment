d505fea5fac6db01c7f4dd6708d861337528d7b7
yakov cohen <yakovc1431@gmail.com>
2022-04-19 22:46:39 +0300

    # Add data hour + events example

---
+++ react-app/src/utils/events.ts
@@ -0,0 +1,7 @@
+export const events = [
+  { id: 1, start: 60, end: 150 },
+  { id: 2, start: 125, end: 180 },
+  { id: 3, start: 180, end: 240 },
+  { id: 4, start: 240, end: 300 },
+  { id: 5, start: 300, end: 360 },
+];
